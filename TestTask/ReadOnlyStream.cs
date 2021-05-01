9a68b649f1f194dc80f67ee83d4d20783e0d4096
vsprog <chmolekula@gmail.com>
2021-05-02 00:01:20 +0700

    File reader.

--- TestTask/ReadOnlyStream.cs
+++ TestTask/ReadOnlyStream.cs
@@ -4,8 +4,9 @@ using System.IO;
 namespace TestTask
 {
     public class ReadOnlyStream : IReadOnlyStream
-    {
-        private Stream _localStream;
+    {        
+        private StreamReader _localStream;
+        private bool _disposed = false;
 
         /// <summary>
         /// Конструктор класса. 
@@ -15,21 +16,15 @@ namespace TestTask
         /// <param name="fileFullPath">Полный путь до файла для чтения</param>
         public ReadOnlyStream(string fileFullPath)
         {
-            IsEof = true;
-
-            // TODO : Заменить на создание реального стрима для чтения файла!
-            _localStream = null;
+            IsEof = false;
+            _localStream = new StreamReader(fileFullPath);
         }
                 
         /// <summary>
         /// Флаг окончания файла.
         /// </summary>
-        public bool IsEof
-        {
-            get; // TODO : Заполнять данный флаг при достижении конца файла/стрима при чтении
-            private set;
-        }
-
+        public bool IsEof { get; private set; }
+        
         /// <summary>
         /// Ф-ция чтения следующего символа из потока.
         /// Если произведена попытка прочитать символ после достижения конца файла, метод 
@@ -38,8 +33,19 @@ namespace TestTask
         /// <returns>Считанный символ.</returns>
         public char ReadNextChar()
         {
-            // TODO : Необходимо считать очередной символ из _localStream
-            throw new NotImplementedException();
+            if (IsEof)
+            {
+                throw new IOException("Достигнут конец файла");
+            }
+
+            char result = (char)_localStream.Read();
+
+            if (_localStream.Peek() < 0)
+            {
+                IsEof = true;
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -53,8 +59,42 @@ namespace TestTask
                 return;
             }
 
-            _localStream.Position = 0;
+            _localStream.DiscardBufferedData();
+            _localStream.BaseStream.Seek(0, SeekOrigin.Begin);
+
             IsEof = false;
+        }
+
+        /// <summary>
+        /// Освобождение ресурсов
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);            
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            
+            if (disposing)
+            {
+                _localStream?.Close();
+            }                
+            
+            _disposed = true;            
+        }
+
+        /// <summary>
+        /// Метод финализации.
+        /// </summary>
+        ~ReadOnlyStream()
+        {
+            Dispose(false);
         }
     }
 }
