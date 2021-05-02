1d8802e8ae06cc3c12df1fe9eda04f80cfb2c5b8
vsprog <chmolekula@gmail.com>
2021-05-02 21:07:44 +0700

    Comments.

--- TestTask/Constants.cs
+++ TestTask/Constants.cs
@@ -5,12 +5,24 @@
     /// </summary>
     public static class Constants
     {
+        /// <summary>
+        /// Регулярное выражение для русскоязычных гласных.
+        /// </summary>
         public const string VowelPattern = @"[аоуэиыеёяю]";
 
+        /// <summary>
+        /// Регулярное выражение для русскоязычных согласных.
+        /// </summary>
         public const string ConsonantsPattern = @"[йцкнгшщзхфвпрлджбтмсч]";
-        
+
+        /// <summary>
+        /// Регулярное выражение для кириллицы.
+        /// </summary>
         public const string CyrillicPattern = @"\p{IsCyrillic}";
-        
+
+        /// <summary>
+        /// Регулярное выражение для дубликатов символов.
+        /// </summary>
         public const string DuplicatePattern = @"(\w)\1";
     }
 }
